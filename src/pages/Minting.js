e2c1f6080000734b43b14135f6b89cf1f6fd1aba
ArbitechSolution <arbitechsolutions@gmail.com>
2022-06-25 11:40:11 +0500

    fianl

--- src/pages/Minting.js
+++ src/pages/Minting.js
@@ -3,8 +3,10 @@ import Footer from "../Component/Footer";
 import PrivateSale from "../media/private-sale.png";
 import Girl from "../media/girl.png";
 import { useTranslation } from "react-i18next";
+import { loadWeb3 } from ".././Component/Api/api";
 const Minting = () => {
   let [t, i18n] = useTranslation();
+  // let [account, setAccount] = useState();
   return (
     <>
       <section id="minting">
@@ -20,7 +22,16 @@ const Minting = () => {
               <div className="ps-info-box">
                 <img src={Girl} alt="" />
                 <h4>{t("mintPara1")}</h4>
-                <p>{t("mintPara2")}</p>
+                <p>
+                  {t("mintPara2")}
+                  <a
+                    className="anchorTags"
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSebdQMsSC0QfFD3msXYLRr30C5i9zbzVYD2apyh_h6rJfAKfg/viewform"
+                    target="_blank"
+                  >
+                    {t("mintPara3")}
+                  </a>
+                </p>
               </div>
               <button className="mint-btn">MINT</button>
             </div>
